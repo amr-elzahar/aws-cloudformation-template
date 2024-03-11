8fe4365283a0ffebb52d22730fe2fc315b8c0929
Amr Elzahar <amr92621@gmail.com>
2024-03-11 07:12:12 +0200

    added CF templte, PS script

---
+++ deployment.ps1
@@ -0,0 +1,13 @@
+$ErrorActionPreference = 'stop' # To allow terminating error instead on non-terminating
+
+$StackName = demo-stack
+$TemplateBody = Get-Content -Path cloudformation-temp.yaml -Raw
+
+if ((Get-CFNStack -StackName $StackName)) {
+
+Update-CFNStack -StackName $StackName TemplateBody $TemplateBody
+} else {
+
+New-CFNStack -StackName $StackName TemplateBody $TemplateBody
+
+}
